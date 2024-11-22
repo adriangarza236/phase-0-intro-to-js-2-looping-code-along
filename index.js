81808bbe19bedf3f82348921d7b0987dda2b04b7
Adrian Garza <adriangarza236@yahoo.com>
2024-11-22 14:19:27 -0600

    completed loop lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message)
+    }
+    return messages;
+}
+console.log(writeCards(["Jim", "Pam"], "weeding"));
+
+
+function countDown() {
+    let counter = 10;
+    while (counter >= 0) {
+        console.log(counter --);
+    }
+}
+countDown();
